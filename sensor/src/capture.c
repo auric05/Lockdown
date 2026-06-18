6df0f648b07c8bf53eb2200e3bf91b36f2539a23
auric05 <bugpendants@gmail.com>
2026-06-18 18:21:56 +1200

    open pcap capture handle on selected interface, null check, closes without error

--- sensor/src/capture.c
+++ sensor/src/capture.c
@@ -28,9 +28,19 @@ int main(int argc, char *argv[]) {
         }
         device = device->next;
     }
+    if(found == 1) {
+        pcap_t *handle = pcap_open_live(device->name, 65536, 1, 1000, errbuf);
+        if (handle == NULL) {
+            fprintf(stderr, "Failed to open capture handle: %s\n", errbuf);
+            pcap_freealldevs(alldevs);
+            return 1;
+        }
+        printf("Capture handle opened on: %s\n", argv[1]);
+        pcap_close(handle);
+    }
     if(found == 0) {
         printf("Interface not found");
-        }
-        pcap_freealldevs(alldevs);
-        return 0;
+    }
+    pcap_freealldevs(alldevs);
+    return 0;
 }
