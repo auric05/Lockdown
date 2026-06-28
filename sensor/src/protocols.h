febddc1ca5b7fe6aaa83c4e38d3ed0ddb481f9b7
auric05 <bugpendants@gmail.com>
2026-06-28 13:59:16 +1200

    add ip_header struct to protocols.h, verify 20-byte packet size

--- sensor/src/protocols.h
+++ sensor/src/protocols.h
@@ -9,4 +9,17 @@ struct ether_header {
     uint16_t ether_type;
 } __attribute__((packed));
 
+struct ip_header {
+    uint8_t vhl;
+    uint8_t tos;
+    uint16_t total_len;
+    uint16_t id;
+    uint16_t frag_off;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t checksum;
+    uint32_t src_ip;
+    uint32_t dest_ip;
+} __attribute__((packed));
+
 #endif
--- sensor/src/test_sizes.c
+++ sensor/src/test_sizes.c
@@ -3,5 +3,6 @@
 
 int main(void) {
     printf("ether_header: %zu bytes\n", sizeof(struct ether_header));
+    printf("ip_header: %zu bytes\n", sizeof(struct ip_header));
     return 0;
 }
